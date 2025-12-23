30df3106814773c9f0fe75b06b14bd4a8b922c35
Hirakari Akshitha <hirakariakshitha@gmail.com>
2025-12-23 18:43:33 +0530

    Refactor polynomial regression code for clarity

--- polynomial regression.py
+++ polynomial regression.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 
 # read the dataset
-dataset=pd.read_csv(r'C:\Users\ADMIN\Downloads\emp_sal.csv')
+dataset=pd.read_csv('emp_sal.csv')
 
 # x and y variables
 x=dataset.iloc[: , 1:2].values
@@ -46,6 +46,7 @@ lin_reg_pred
 
 poly_reg_pred = lin_reg_2.predict(poly_reg.fit_transform([[6.5]]))
 poly_reg_pred
+
 
 
 
